7374780104eb02e48ffbf69649e53c1d976292a9
Azikenp <azikenp26@gmail.com>
2023-08-12 21:35:27 +0100

    new project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,10 @@ function Navbar() {
         setClick(prev => !prev)
     }
 
+    const closeMobileMenu = () => {
+        setClick(false)
+    }
+
     return (
         <>
             <nav className="navbar">
@@ -20,7 +24,24 @@ function Navbar() {
                     </div>
                     <ul className={click? 'nav-menu active': 'nav-menu'}>
                         <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}></Link>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
                         </li>
                     </ul>
                 </div>
