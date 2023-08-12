25780d703068aa70e15dad89118f4b77d13d501e
Azikenp <azikenp26@gmail.com>
2023-08-12 21:50:49 +0100

    new

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+import {Link} from 'react-router-dom';
+
+
+export default const Button = ({children, type, onClick, buttonStylem, buttonSize}) => {
+    return
+}
