1d1c2fb2d5d9b73479c86a220b658b40ab0cf02c
Azikenp <azikenp26@gmail.com>
2023-08-13 14:58:37 +0100

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Products from './components/pages/products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUP';
 
 function App() {
 
@@ -13,6 +16,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact Component={Home}/>
+          <Route path='/services' exact Component={Services}/>
+          <Route path='/products' exact Component={Products}/>
+          <Route path='/sign-up' exact Component={SignUp}/>
         </Routes>
       </Router>
     </>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -15,6 +15,32 @@ function Cards() {
                      label='Adventure'
                      path='/services'
                     />
+                     <CardItems
+                     src='/images/img-2.jpg'
+                     text='Travel through the islands of Bali in a private cruise'
+                     label='Luxury'
+                     path='/services'
+                    />
+                </ul>
+                <ul className='cards__items'>
+                <CardItems
+                     src='/images/img-3.jpg'
+                     text='Set Sail In The Atlantic Ocean Visiting Uncharted Waters'
+                     label='Mystery'
+                     path='/services'
+                    />
+                     <CardItems
+                     src='/images/img-4.jpg'
+                     text='Experience Football On Top Of The Himilayan Mountains'
+                     label='Adventure'
+                     path='/services'
+                    />
+                     <CardItems
+                     src='/images/img-8.jpg'
+                     text='Ride Through The Sahara Desert on a guided Camel Tour'
+                     label='Adrenaline'
+                     path='/services'
+                    />
                 </ul>
             </div>
         </div>
